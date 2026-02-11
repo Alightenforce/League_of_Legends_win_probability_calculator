48f1e01ab05443055c8630b9af98662816c2f769
Alightenforce <alexking1uk@gmail.com>
2026-02-11 22:15:18 +0000

    Mapped champion name to mastery points

--- LoL_Win_Probability_Calculator.py
+++ LoL_Win_Probability_Calculator.py
@@ -1,13 +1,9 @@
 from Player_Class import Player
 
 def main():
-    player1 = Player("Alightenforce", "4040", "europe", 5, "12.6.1")
-    # player1.print_player_data()
-    # player1.print_win_rate()
-    link = player1.get_link_for_player_mastery()
-    mastery_data = player1.get_all_champion_masteries(link)
-    print(mastery_data)
-
+    player1 = Player("Alightenforce", "4040", "europe", 10, "12.6.1")
+    player1.print_win_rate()
+    player1.print_champion_name_to_champion_mastery()
 
 if __name__ == "__main__":
     main()
--- Player_Class.py
+++ Player_Class.py
@@ -31,19 +31,19 @@ class Player:
         print(self.count)
         print(self.version_number)
 
-    def get_link_for_puuid(self):
+    def get_link_for_puuid(self) -> str:
         return f"https://{self.region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{self.summoner_name}/{self.summoner_tag}?api_key={API_KEY}"
 
-    def get_link_for_region_code(self):
+    def get_link_for_region_code(self) -> str:
         return f"https://{self.region}.api.riotgames.com/riot/account/v1/region/by-game/lol/by-puuid/{self.puuid}?api_key={API_KEY}"
 
-    def get_link_for_player_match_history(self):
+    def get_link_for_player_match_history(self) -> str:
         return f"https://{self.region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{self.puuid}/ids?start=0&count={self.count}&api_key={API_KEY}"
 
-    def get_link_for_player_mastery(self):
+    def get_link_for_player_mastery(self) -> str:
         return f"https://{self.region_code}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{self.puuid}?api_key={API_KEY}"
 
-    def get_link_for_data_dragon(self):
+    def get_link_for_data_dragon(self) -> str:
         return f"http://ddragon.leagueoflegends.com/cdn/{self.version_number}/data/en_US/champion.json"
 
     def get_variable_from_link(self, link: str, variable: str) -> str:
@@ -52,13 +52,13 @@ class Player:
         return_variable = page_json[variable]
         return return_variable
 
-    def get_puuid_from_link(self, link : str):
+    def get_puuid_from_link(self, link : str) -> str:
         return self.get_variable_from_link(link, "puuid")
 
     def set_puuid(self):
         self.puuid = self.get_puuid_from_link(self.get_link_for_puuid())
 
-    def get_region_code_from_link(self, link : str):
+    def get_region_code_from_link(self, link : str) -> str:
         return self.get_variable_from_link(link, "region")
 
     def set_region_code(self):
@@ -113,7 +113,10 @@ class Player:
         return winrate
 
     def print_win_rate(self):
+        print("--------------------")
+        print("Player's winrate: ")
         print(f"{self.summoner_name}'s win rate is {self.get_win_rate()}% over the past {self.count} matches")
+        print("--------------------")
 
     def get_all_champion_masteries(self, champion_masteries_link : str) -> list:
         list_of_champion_masteries = []
@@ -124,3 +127,36 @@ class Player:
             list_of_champion_masteries.append(id_to_mastery)
         return list_of_champion_masteries
 
+    # Dictionary uses O(1) over a list which would use O(n)
+    def find_champion_ids_to_names(self, data_dragon_link : str) -> dict:
+        dict_of_champion_ids_to_names = {}
+        page = requests.get(data_dragon_link)
+        page_json = json.loads(page.content)
+        all_champion_names = page_json["data"]
+        for champion in all_champion_names.values():
+            dict_of_champion_ids_to_names[int((champion["key"]))] = champion["name"]
+        return dict_of_champion_ids_to_names
+
+    def match_champion_name_to_champion_mastery(self, list_of_champion_masteries : list, dict_of_champion_ids_to_names : dict) -> dict:
+        champion_name_to_champion_mastery = {}
+        for champion_id_mastery, mastery_points in list_of_champion_masteries:
+            if champion_id_mastery in dict_of_champion_ids_to_names:
+                champion_name = (dict_of_champion_ids_to_names[champion_id_mastery])
+                champion_name_to_champion_mastery[champion_name] = mastery_points
+        return champion_name_to_champion_mastery
+
+    def get_champion_name_to_champion_mastery(self) -> dict:
+        link_mast = self.get_link_for_player_mastery()
+        link_dd = self.get_link_for_data_dragon()
+
+        list1 = self.get_all_champion_masteries(link_mast)
+        dict1 = self.find_champion_ids_to_names(link_dd)
+        name_to_mastery_points = self.match_champion_name_to_champion_mastery(list1, dict1)
+        return name_to_mastery_points
+
+    def print_champion_name_to_champion_mastery(self):
+        print("--------------------")
+        print("All player's mastery: ")
+        for name, points in self.get_champion_name_to_champion_mastery().items():
+            print(f"{name}: {points}")
+        print("--------------------")
